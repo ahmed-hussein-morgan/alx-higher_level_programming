4bb4d5974d68615cc3037c8664cee4e960954a9d
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-01-05 19:56:06 +0000

    add my own answer 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-# 100-print_tebahpla.py
 
-""""Print the alphabet in reverse order alternating upper- and lower-case."""
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+"""
+program that prints the ASCII alphabet, in reverse order,\
+        alternating lowercase and uppercase
+        (z in lowercase and Y in uppercase)
+        - You can only use one print function with string format
+        - You can only use one loop in your code
+        - You are not allowed to store characters in a variable
+        - You are not allowed to import any module
+"""
+
+for letter in range(ord('z'), ord('a')-1, -1):
+
+    if letter % 2:
+        new_letter = letter - 32
+    else:
+        new_letter = letter
+    print("{}".format(chr(new_letter)), end="")
