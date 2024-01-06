372f13542976dd9fa7e21fe5540142699aa1161a
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-01-06 12:19:19 +0000

    5-no_c.py - Done

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    """
+    A function that removes all characters c and C from a string\
+            The function should return the new string
+            You are not allowed to import any module
+            You are not allowed to use str.replace()
+    """
+    new_word = ""
+
+    string = list(my_string)
+
+    for i in range(len(string)):
+        if string[i] == 'c' or string[i] == 'C':
+            continue
+        else:
+            new_word += string[i]
+
+    return new_word
