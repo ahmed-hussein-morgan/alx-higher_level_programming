0bc5f5c01510b607f2b58fbbd4d85434266b0eb7
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-01-22 20:42:16 +0200

    2-safe_print_list_integers.py - fix pycodestyle

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
     printed_num = 0
     for i in range(x):
         try:
-            #y = my_list[i]
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end='')
                 printed_num += 1
