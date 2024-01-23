6644df2e40d55276d4a1f85dec8c25dd97b8957f
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-01-23 19:34:18 +0200

    task 1 + task 2 Done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square the number. """
+
+
+class Square:
+    """Define a square class. """
+
+    def __init__(self, size):
+        """
+        Parameters
+        ----------
+            size : the size of the square.
+
+        """
+
+        self.__size = size
