2a2e3ed8ed7dec0f462c9677c080d2477d67a2f9
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-01-25 04:59:24 +0200

    102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,57 @@
+class Square:
+    """Define a square class. """
+
+    def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+            size : the size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculating the current square area"""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        if isinstance(other, Square):
+            return self.area() == other.area()
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, Square):
+            return self.area() != other.area()
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Square):
+            return self.area() > other.area()
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, Square):
+            return self.area() >= other.area()
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Square):
+            return self.area() < other.area()
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Square):
+            return self.area() <= other.area()
+        return NotImplemented
