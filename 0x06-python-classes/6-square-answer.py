09955725be945ba887eb8e518ebdae0d29606b0d
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-01-27 04:49:21 +0200

    6-square.py - retry

---
+++ 0x06-python-classes/6-square-answer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""this defines the square"""
+class Square:
+    """Define a square class. """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Parameters
+        ----------
+            size : the size of the square.
+            position : the position of the square.
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """Calculating the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(' ' * self.__position[0] + '#' * self.__size)
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""this defines the square"""
+"""Square the number. """
+
+
 class Square:
     """Define a square class. """
 
@@ -8,10 +10,39 @@ class Square:
         Parameters
         ----------
             size : the size of the square.
-            position : the position of the square.
+
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
         self.__size = size
+
+        # if not isinstance(position, tuple) and len(position) != 2:
+        #     if position[0] < 0 or position[1] < 0:
+        #         raise TypeError("position must be a tuple of 2 positive integers")
+            
         self.__position = position
+
+    
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2 and value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        
+        self.__position = value
+       
+
+    def area(self):
+        """Calculating the current square area"""
+        return self.__size ** 2
 
     @property
     def size(self):
@@ -26,24 +57,10 @@ class Square:
         else:
             self.__size = value
 
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) and i >= 0 for i in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
-
-    def area(self):
-        """Calculating the current square area"""
-        return self.__size ** 2
-
     def my_print(self):
         if self.__size == 0:
             print()
+
         else:
             for _ in range(self.__position[1]):
                 print()
