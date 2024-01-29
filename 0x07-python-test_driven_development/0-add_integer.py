8a61f87aec26379bcb4c0b139c0ff987b744cb58
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-01-29 07:05:09 +0200

    task 0 - without all test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,30 +3,18 @@
 
 
 def add_integer(a, b=98):
-	"""a function that adds 2 integers\
-		Returns an integer: the addition of a and b
-	"""
-	# try:
-	# 	# if isinstance(a, int) or isinstance(a, float):
-	# 	a = int(a)
-	# except TypeError:
-	# 	print("a must be an integer")
-	# 	return None
-	# try:
-	# 	# if isinstance(b, int) or isinstance(b, float):
-	# 	b = int(b)
-	# except TypeError:
-	# 		print("b must be an integer")
-	# 		return None
-	# return a + b
-	if isinstance(a, int) or isinstance(a, float):
-		a = int(a)
-	else:
-		raise TypeError("a must be an integer")
+    """a function that adds 2 integers\
+        Returns an integer: the addition of a and b
+    """
 
-	if isinstance(b, int) or isinstance(b, float):
-		b = int(b)
-	else:
-		raise TypeError("b must be an integer")
-	
-	return a + b
+    if isinstance(a, int) or isinstance(a, float):
+        a = int(a)
+    else:
+        raise TypeError("a must be an integer")
+
+    if isinstance(b, int) or isinstance(b, float):
+        b = int(b)
+    else:
+        raise TypeError("b must be an integer")
+
+    return a + b
