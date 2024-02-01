f9770f2d34ef54f5b24a77fc04940b1cc592d70c
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-02-01 09:04:59 +0200

    bgfgdf

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,12 +5,13 @@
 class LockedClass:
     """lock class"""
 
-    def __new__(cls):
-        obj = super().__new__(cls)
-        return obj
+    # def __new__(cls):
+    #     obj = super().__new__(cls)
+    #     return obj
 
     def __setattr__(self, name, value):
         if name != 'first_name':
-            raise AttributeError(f"'{self.__class__.__name__}'object has no attribute '{name}'")
+            raise AttributeError(f"'{self.__class__.__name__}\
+                                 'object has no attribute '{name}'")
         else:
             super().__setattr__(name, value)
