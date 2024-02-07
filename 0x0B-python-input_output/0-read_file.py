50750843a36d79c75eeabe78cc94d9ec1e563858
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-02-07 02:48:36 +0200

    read the file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@
 
 def read_file(filename=""):
     """raed a file"""
-    with open('filename', 'r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         reading_file = f.read()
