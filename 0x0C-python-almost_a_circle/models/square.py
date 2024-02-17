a30b5585edbfaf318615301fba5731267db218d9
Ahemd Hussein Morgan <136130399+ahmed-hussein-morgan@users.noreply.github.com>
2024-02-17 02:53:15 +0000

    task - 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,22 +8,20 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initializing thw square"""
         super().__init__(size, size, x, y, id)
-        self.size = size
-        #self.size_height = size
-        # self.height = size
+        # self.size = size
 
     @property
     def size(self):
-        return self.__size
-    
+        return self.width
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__size = value
-
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """overriding the __str__ method of the reqtangle parent class"""
