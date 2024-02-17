1846ba07c91eb6d433b6b582e724fff2a249f558
Ahemd Hussein Morgan <136130399+ahmed-hussein-morgan@users.noreply.github.com>
2024-02-17 03:00:03 +0000

    z\x\

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,28 +1,3 @@
-#!/usr/bin/python3
-"""the inhereted square module"""
-from models.rectangle import Rectangle
-
-
-class Square(Rectangle):
-    """the square chape class"""
-    def __init__(self, size, x=0, y=0, id=None):
-        """initializing thw square"""
-        super().__init__(size, size, x, y, id)
-        # self.size = size
-
-    @property
-    def size(self):
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.width = value
-        self.height = value
-
-    def __str__(self):
-        """overriding the __str__ method of the reqtangle parent class"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
