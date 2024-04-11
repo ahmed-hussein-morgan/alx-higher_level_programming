ef9ed75b25a026414c764e4960829255c1b7cc09
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-04-12 00:25:52 +0200

    fix stile and chmod for all

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -2,13 +2,12 @@
 
 // replace the value 12 with 89
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  
-  // update the needed key (value) of the dictionary
-  myObject.value = 89;
-  
-  console.log(myObject);
-  
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// update the needed key (value) of the dictionary
+myObject.value = 89;
+
+console.log(myObject);
