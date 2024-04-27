39ed2ab3bd5948f63d1ef3cef353e5473a0d1ae7
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-04-27 05:33:18 +0300

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,5 +2,12 @@
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 
 import urllib.request
+
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
+
+# Display the response body in the specified format
+print("Body response:")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode('utf-8')))
