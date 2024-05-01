96ffd18e1ff8ee43f9902a1f9478adedbddcb78f
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-01 05:27:06 -0700

    101 try 03

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request with the contents of a file, passed with the filename as the second argument
-curl -s -F "@$2;type=json" $1 
+echo "$(cat "$2" | sed 's/,\s*($|})/\\1/g')" | curl -s -H "Content-Type: application/json" -d @- "$1"
