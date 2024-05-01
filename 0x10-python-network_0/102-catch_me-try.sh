de844d93dd363ca89688c5fa571c7ab061f08df2
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-01 06:44:40 -0700

    102 - try 05

--- 0x10-python-network_0/102-catch_me-try.sh
+++ 0x10-python-network_0/102-catch_me-try.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # make a request to 0.0.0.0:5000/catch_me respond with a message containing You got me!
-curl -s -o /dev/null -w 'You got me!' 0.0.0.0:5000/catch_me
+curl -s 0.0.0.0:5000/catch_me | grep -q "You got me!" && printf "You got me!\n"
