8d2d196ad7f84f0fc0bc922e46bb2767492a6256
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-19 16:14:33 +0300

    task 03 - try 00

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""catch the error"""
+
+from urllib.request import urlopen
+import sys
+from urllib.error import HTTPError, URLError
+
+
+if __name__ == "__main__":
+    try:
+        with urlopen(sys.argv[1]) as response:
+            html = response.read()
+
+        print(f"html")
+
+    except HTTPError as error:
+        print(f"Error code: {error.code} {error.reason}")
