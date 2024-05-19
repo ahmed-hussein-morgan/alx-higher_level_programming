f3449150fa0684fa4b5417d78b18982748737bdc
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-19 16:54:53 +0300

    task 04 - try 01

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ response = requests.get("https://alx-intranet.hbtn.io/status")
 decode_content = response.content.decode('utf-8')
 # print(dir(requests.get))
 print(f"Body response:\n\t- type: {type(decode_content)}\n\
-      \t- content: {decode_content}")
+\t- content: {decode_content}")
