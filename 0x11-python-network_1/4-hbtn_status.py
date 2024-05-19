e53854caf2c5f5f6ef048ce10d824a4f05a364a5
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-19 16:53:28 +0300

    task 04 - try 00

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""start using request package"""
+import requests
+
+response = requests.get("https://alx-intranet.hbtn.io/status")
+decode_content = response.content.decode('utf-8')
+# print(dir(requests.get))
+print(f"Body response:\n\t- type: {type(decode_content)}\n\
+      \t- content: {decode_content}")
