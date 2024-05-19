9cae41bff7501e8f7dd9b6b2f21e4f11406155a9
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-19 17:23:56 +0300

    task 05 - try 00

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""displays the value of the variable X-Request-Id"""
+import requests
+import sys
+
+response = requests.get(sys.argv[1]).headers
+print(response.get('X-Request-Id'))
