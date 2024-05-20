62af412313e7d86cdc1f1c5a8b80d5a930afc855
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-20 15:00:13 +0300

    task 08 - try 10

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,14 +6,14 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        query = sys.argv[1]
-        if len(query) <= 1 or not isinstance(query, chr):
-            response = requests.post('http://0.0.0.0:5000/search_user', data={"q": ""})  # Changed to POST
-        else:
-            response = requests.post('http://0.0.0.0:5000/search_user', data={"q": query})  # Changed to POST
-    else:
+    # if len(sys.argv) > 1:
+    query = sys.argv[1]
+    if len(sys.argv) <= 1 or not isinstance(query, chr):
         response = requests.post('http://0.0.0.0:5000/search_user', data={"q": ""})  # Changed to POST
+    else:
+        response = requests.post('http://0.0.0.0:5000/search_user', data={"q": "{query}"})  # Changed to POST
+    # else:
+    #     response = requests.post('http://0.0.0.0:5000/search_user', data={"q": ""})  # Changed to POST
 
     # Check if the response status code indicates success
     if response.status_code == 200:
