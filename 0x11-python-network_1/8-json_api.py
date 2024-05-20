a64f19879534678077a2dcbee6522de6481d1593
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-20 14:38:44 +0300

    task 08 - try 08 (check the status code before converting to json)

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         query = sys.argv[1]
         if len(query) <= 1 or not isinstance(query, str):
-            response = requests.get('http://0.0.0.0:5000/search_user', params={'q': ""})
+            response = requests.post('http://0.0.0.0:5000/search_user', data={"q": ""})  # Changed to POST
         else:
-            response = requests.get('http://0.0.0.0:5000/search_user', params={'q': query})
+            response = requests.post('http://0.0.0.0:5000/search_user', data={"q": query})  # Changed to POST
     else:
-        response = requests.get('http://0.0.0.0:5000/search_user', params={'q': ""})
+        response = requests.post('http://0.0.0.0:5000/search_user', data={"q": ""})  # Changed to POST
 
     # Check if the response status code indicates success
     if response.status_code == 200:
