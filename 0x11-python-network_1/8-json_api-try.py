a30160636afc087c565af2ee6f9ff9bcb455263e
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-20 14:26:43 +0300

    task 08 - try 06

--- 0x11-python-network_1/8-json_api-try.py
+++ 0x11-python-network_1/8-json_api-try.py
@@ -9,12 +9,17 @@ if __name__ == "__main__":
     if len(sys.argv) <= 1 or not isinstance(sys.argv[1], chr):
         response = requests.\
             get('http://0.0.0.0:5000/search_user', params={'q': ""})
+
+        print("No result")
+
+        
     else:
         response = requests.\
             get('http://0.0.0.0:5000/search_user', params={'q': {sys.argv[1]}})
-    if not response.json():
-        print("Not a valid JSON")
-    elif len(response.json()) == 0:
-        print("No result")
-    else:
-        print(f"[{response.json([id])}] {response.json([__name__])}")
+        
+        if not response.json():
+            print("Not a valid JSON")
+        elif len(response.json()) == 0:
+            print("No result")
+        else:
+            print(f"[{response.json([id])}] {response.json([__name__])}")
