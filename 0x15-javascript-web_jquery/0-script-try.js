e57e79ae83d3c55e651d3da7853d224ec8a0c766
ahmed-hussein-morgan <ahmed.h.morgan2050@gmail.com>
2024-05-29 17:57:07 +0300

    task 0 - try 00

---
+++ 0x15-javascript-web_jquery/0-script-try.js
@@ -0,0 +1,12 @@
+document.querySelector("header");
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>;
+
+<script>
+    $function() {
+       $("header").css({
+        color: 'red'
+       })
+    });
+
+</script>
---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+// Select the header element using document.querySelector
+const headerElement = document.querySelector("header");
+
+if (headerElement) {
+    headerElement.style.color = '#FF0000';
+}
